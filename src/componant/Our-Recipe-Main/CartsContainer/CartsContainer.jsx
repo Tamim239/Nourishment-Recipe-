c120402755435c25e7b077c1abbb8d54c451bb7d
“Tamim239” <mdibrahimislamtamim239@gmail.com>
2024-03-17 12:08:19 +0600

    fixed some problem

--- src/componant/Our-Recipe-Main/CartsContainer/CartsContainer.jsx
+++ src/componant/Our-Recipe-Main/CartsContainer/CartsContainer.jsx
@@ -1,11 +1,11 @@
 import PropTypes from "prop-types";
 export const CartsContainer = ({card, handlePreparing, cart, prepareTime, totalCalories}) => {
   return (
-    <div className="md:w-2/5 ml-5  p-2 border rounded-2xl">
+    <div className="md:w-2/5 md:ml-5  p-2 border rounded-2xl">
       <h1 className="text-center text-2xl font-semibold p-5 border-b ">
         Want to cook: {card.length}
       </h1>
-      <div className="">
+      <div className="w-1/2">
         <table className="table">
           {/* head */}
           <thead>
@@ -14,10 +14,10 @@ export const CartsContainer = ({card, handlePreparing, cart, prepareTime, totalC
               <th>Name</th>
               <th>Time</th>
               <th>Calories</th>
-              <th></th>
+               <th></th>
             </tr>
           </thead>
-          <tbody className="pr-5 ">
+          <tbody className="md:pr-5">
             {/* row */}
             {
               card.map((card, index) => <tr key={card.id}>
